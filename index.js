73fe48b72938ba9463f5df11815c34e069fd1668
Philipp Mainz <pmainz@varion.de>
2017-11-14 13:22:44 +0100

    added functionality

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+var data = {
+    names: require('./names.json'),
+    adjectives: require('./adjectives.json')
+  },
+  seperator = '-';
+module.exports.setNames = function (names) {
+  data.names = names;
+};
+module.exports.setAdjectives = function (adjectives) {
+  data.adjectives = adjectives;
+};
+module.exports.setSeperator = function (new_seperator) {
+  seperator = new_seperator;
+};
+
+module.exports.generate = function () {
+  var ran_a = Math.floor(Math.random() * data.names.length),
+    ran_b = Math.floor(Math.random() * data.adjectives.length),
+    ran_suffix = Math.random() * 100;
+  return data.names[ran_a] + seperator + data.adjectives[ran_b] + seperator + ran_suffix;
+};
