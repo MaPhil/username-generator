1f90718b9cd362b0e2c5e0620d920397816c1cf2
Jérémie Zarca <jzarca01@users.noreply.github.com>
2018-09-14 17:02:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 'use strict';
-var data = {
+
+const data = {
     names: require('./names.json'),
     adjectives: require('./adjectives.json')
-  },
-  seperator = '-';
-module.exports.setNames = function (names) {
-  data.names = names;
-};
-module.exports.setAdjectives = function (adjectives) {
-  data.adjectives = adjectives;
-};
-module.exports.setSeperator = function (new_seperator) {
-  seperator = new_seperator;
+}
+let seperator = '-'
+
+function setNames(names) {
+    data.names = names
 };
 
-module.exports.generate = function () {
-  var ran_a = Math.floor(Math.random() * data.names.length),
-    ran_b = Math.floor(Math.random() * data.adjectives.length),
-    ran_suffix = Math.floor(Math.random() * 100);
-  return  data.adjectives[ran_b]+ seperator + data.names[ran_a] + seperator + ran_suffix;
+function setAdjectives(adjectives) {
+    data.adjectives = adjectives
 };
+
+function setSeperator(new_seperator) {
+    seperator = new_seperator
+};
+
+function generate() {
+    const ran_a = Math.floor(Math.random() * data.names.length)
+    const ran_b = Math.floor(Math.random() * data.adjectives.length)
+    const ran_suffix = Math.floor(Math.random() * 100)
+    return `${data.adjectives[ran_b]}${seperator}${data.names[ran_a]}${ran_suffix}`
+};
+
+module.exports = {
+    setNames: setNames,
+    setAdjectives: setAdjectives,
+    setSeperator: setSeperator,
+    generate: generate
+}
