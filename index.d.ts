057a010285301a6a0391a5e8af6adf9a21f2a733
Itay Ganor <6430313+itayganor@users.noreply.github.com>
2020-04-26 14:12:45 +0300

    Added typings

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,4 @@ export function setAdjectives(adjectives: string[]): void;
 
 export function setSeparator(newSeparator: string): void;
 
-export function generate(): string;
+export function generate(addSuffix?: boolean): string;
